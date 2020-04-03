ed8948adfdb5b2a669e2714ff2b61f090ee48481
Christopher Heyder <christopherheyder@mail.de>
2020-04-03 16:52:29 +0200

    Get all cards

--- slim-server/index.php
+++ slim-server/index.php
@@ -5,32 +5,28 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use Middlewares\TrailingSlash;
 
+
 require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
 $app->add(new TrailingSlash(false)); // remove trailing slashes
 
-// GET /
 $app->get('/', function (Request $request, Response $response, array $args) {
-    // TODO serve the html for the react app instead of "Hello world!"
     $response->getBody()->write(file_get_contents('../client/build/index.html'));
-
     return $response;
 });
 
-// GET /cards
-$app->get('/', function (Request $request, Response $response, array $args) {
-    // TODO serve the html for the react app instead of "Hello world!"
-    $response->getBody()->write(file_get_contents('../client/build/index.html'));
-
+$app->get('/cards', function (Request $request, Response $response, array $args) {
+    $dbconnect = (new MongoDB\Client("mongodb://localhost:27017/flashcards-react-jerry"));
+    $collection = $dbconnect->selectCollection('flashcards-react-jerry', 'cards'); 
+    $cursor = $collection->find([]);
+    $response->getBody()->write(json_encode($cursor->toArray()));
     return $response;
 });
 
 // GET /cards/{id}
-$app->get('/:id', function (Request $request, Response $response, array $args) {
-    // TODO serve the html for the react app instead of "Hello world!"
+$app->get('/cards/{id}', function (Request $request, Response $response, array $args) {
     $response->getBody()->write(file_get_contents('../client/build/index.html'));
-
     return $response;
 });
 
