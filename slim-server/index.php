323cd6c006f1c1d6588024e4a58b25f7263da692
Christopher Heyder <christopherheyder@mail.de>
2020-04-04 12:59:35 +0200

    Post works after reload, patch doesnt update db

--- slim-server/index.php
+++ slim-server/index.php
@@ -4,12 +4,13 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use Middlewares\TrailingSlash;
-
+use MongoDB\BSON\ObjectId;
 
 require __DIR__ . '/vendor/autoload.php';
 
+
 $app = AppFactory::create();
-$app->add(new TrailingSlash(false)); // remove trailing slashes
+$app->add(new TrailingSlash(false));
 
 $app->get('/', function (Request $request, Response $response, array $args) {
     $response->getBody()->write(file_get_contents('../client/build/index.html'));
@@ -24,57 +25,59 @@ $app->get('/cards', function (Request $request, Response $response, array $args)
     return $response;
 });
 
-// GET /cards/{id}
-
+//Don't know how to test this.
 $app->get('/cards/{id}', function (Request $request, Response $response, array $args) {
     $dbconnect = (new MongoDB\Client("mongodb://localhost:27017/flashcards-react-jerry"));
     $collection = $dbconnect->selectCollection('flashcards-react-jerry', 'cards'); 
     $cursor = $collection->findOne([
-        '_id' => $args['1'],
+        '_id' => $args['id'],
     ]);
-
     $response->getBody()->write(json_encode($cursor));
     return $response;
 });
 
-// POST /cards
-//request body {"question":"Hallo","answer":"Du","tags":["js"]}
+//Not a clue why this is working (after reload)
 $app->post('/cards', function ($request, $response, $args) {
     $dbconnect = (new MongoDB\Client("mongodb://localhost:27017/flashcards-react-jerry"));
     $collection = $dbconnect->selectCollection('flashcards-react-jerry', 'cards'); 
-    $postContent = $request->getParsedBody();
-    /* NICHT AUSPROBIEREN, auf jeden FAll nur in abgwandelter Form
-    //hat beim letzten MAl alles zerschossen
-    $cursor = $collection->insertOne([
-        'question' => $args['question'],
-        'answer' => $args['answer'],
-        'tags' => $args['tags']
+    $postArray = $request->getParsedBody();
+    $collection->insertOne($postArray);
+
+    /* Why does the above insert work, but not the lower one 
+    $collection->insertOne([
+        'question' => $postArray['question'],
+        'answer' => $postArray['answer'],
+        'tags' => $postArray['tags']
     ]);
     */
-    $response->getBody()->write($postContent);
+
+    /* Why does this missing throws a 500, but everything else is still working (sometimes)?*/
+    $html = var_export($postArray, true);
+    $response->getBody()->write($html);
+    return $response;
+    
 });
 
 
-/*
+//Throws 200, response is fine, but not loaded from/written to db.
 $app->patch('/cards/{id}', function ($request, $response, $args) {
     $dbconnect = (new MongoDB\Client("mongodb://localhost:27017/flashcards-react-jerry"));
     $collection = $dbconnect->selectCollection('flashcards-react-jerry', 'cards'); 
-    $cursor = $collection->updateOne(
-        [
-            '_id' => $args['1'],
-        ], 
-        [
-            $args['1'],
-        ]);
-    $response->getBody()->write(json_encode($cursor));
+    $postArray = $request->getParsedBody();
+    $collection->updateOne(
+        ['_id' => $args['id']], 
+        ['$set' => $postArray]);
+    $html = var_export($postArray, true);
+    $response->getBody()->write($html);
     return $response;
 });
-*/
+
+
 
 // DELETE /cards/all
 
 // DELETE /cards/{id}
 
-
+$app->addBodyParsingMiddleware(); //What is this Middleware for?
 $app->addErrorMiddleware(true, false, false);
 $app->run();
